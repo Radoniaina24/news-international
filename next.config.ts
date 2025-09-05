2037c78f4ef52a40772627f4d366f8f6ed74ecec
Radoniaina24 <andriambolaradoniainamichael@gmail.com>
2025-09-05 14:45:00 +0300

    v0.2

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: [
+      "localhost",
+      "images.pexels.com",
+      "res.cloudinary.com",
+      "via.placeholder.com",
+      "source.unsplash.com",
+      "flagcdn.com",
+      "gateofafrica.com",
+    ],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        port: "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
