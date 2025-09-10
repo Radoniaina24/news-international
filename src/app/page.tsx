0ae10aeff0d0aeef707126db21ad8f708d55c47e
Radoniaina24 <andriambolaradoniainamichael@gmail.com>
2025-09-10 08:15:27 +0300

    v2.5

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,46 +2,17 @@
 import BannerCarousel from "@/components/Banner/BannerCarousel";
 import CategoriesSection from "@/components/Home/CategoriesSection";
 import HeroSection from "@/components/Home/HeroSection";
-import TechnologySection from "@/components/Home/TechnologySection";
 
 import NewsletterSignup from "@/components/Newsletter/NewsletterSignup";
 
 export default function Home() {
-  const ads = [
-    {
-      id: 1,
-      title: "Promotion Exceptionnelle",
-      description:
-        "Profitez de nos offres spéciales avec des réductions allant jusqu'à 50%",
-      imageUrl: "/images/ad1.jpg",
-      ctaText: "Découvrir",
-      ctaLink: "/promotions",
-    },
-    {
-      id: 2,
-      title: "Nouveaux Produits",
-      description: "Découvrez notre nouvelle collection de produits exclusifs",
-      imageUrl: "/images/ad2.jpg",
-      ctaText: "Voir la collection",
-      ctaLink: "/nouveautes",
-    },
-    {
-      id: 3,
-      title: "Livraison Gratuite",
-      description:
-        "Commandez maintenant et bénéficiez de la livraison gratuite",
-      imageUrl: "/images/ad3.jpg",
-      ctaText: "Commander",
-      ctaLink: "/shop",
-    },
-  ];
   return (
     <div className="min-h-screen bg-gray-50 dark:bg-gray-900 transition-colors">
       <main>
         <BannerCarousel />
         <HeroSection />
         <CategoriesSection />
-        <TechnologySection />
+        {/* <TechnologySection /> */}
         <NewsletterSignup />
         {/* <HtmlContent html={content} /> */}
       </main>
