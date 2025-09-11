08cc48bc5ccd2e3a3d2694c9e4012f977c3fc722
Radoniaina24 <andriambolaradoniainamichael@gmail.com>
2025-09-11 09:58:39 +0300

    v3.3

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -9,7 +9,7 @@ export default function PostPage() {
   const { slug } = useParams<{ slug: string }>(); // récupère l'ID depuis l'URL
 
   const { data: post, error, isLoading } = useGetPostBySlugQuery(slug);
-  console.log(post);
+  // console.log(post);
   if (isLoading) return <BlogSkeleton />;
   if (error) return <p>Erreur lors du chargement de l’article.</p>;
   if (!post) return <p>Aucun article trouvé.</p>;
