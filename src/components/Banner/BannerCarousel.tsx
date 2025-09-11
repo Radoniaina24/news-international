ef7913a7350ba44c32d06fad14fb17c032f64f2d
Radoniaina24 <andriambolaradoniainamichael@gmail.com>
2025-09-11 13:13:31 +0300

    v3.8

--- src/components/Banner/BannerCarousel.tsx
+++ src/components/Banner/BannerCarousel.tsx
@@ -127,7 +127,7 @@ const ProfessionalBanner: React.FC = () => {
 
       {/* Contenu principal */}
       <div className="relative z-10 h-full flex items-center">
-        <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 items-center">
             {/* Section texte */}
             <div
