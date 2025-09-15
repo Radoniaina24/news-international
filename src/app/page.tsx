5bf084390214dccfc294e82b491b820a10f3394f
Radoniaina24 <andriambolaradoniainamichael@gmail.com>
2025-09-15 16:46:45 +0300

    v5.0

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import BannerCarousel from "@/components/Banner/BannerCarousel";
+
 import CategoriesSection from "@/components/Home/CategoriesSection";
 import HeroSection from "@/components/Home/HeroSection";
 
@@ -12,6 +13,34 @@ export default function Home() {
         <BannerCarousel />
         <HeroSection />
         <CategoriesSection />
+        {/* <AnimatedTestimonials
+          data={[
+            {
+              description:
+                "ScrollX-UI has completely transformed how I build interfaces. The animations are silky smooth, and the components are modular and responsive.",
+              image:
+                "https://images.unsplash.com/photo-1611558709798-e009c8fd7706?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3",
+              name: "Isabelle Carlos",
+              handle: "@isabellecarlos",
+            },
+            {
+              description:
+                "I love how ScrollX-UI makes my projects look professional with minimal effort. The documentation is clear and the community is super helpful.",
+              image:
+                "https://plus.unsplash.com/premium_photo-1692340973636-6f2ff926af39?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3",
+              name: "Lana Akash",
+              handle: "@lanaakash",
+            },
+            {
+              description:
+                "The smooth scrolling animations and intuitive components in ScrollX-UI save me hours of development time!",
+              image:
+                "https://images.unsplash.com/photo-1552374196-c4e7ffc6e126?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3",
+              name: "Liam O’Connor",
+              handle: "@liamoc",
+            },
+          ]}
+        /> */}
         <NewsletterSignup />
       </main>
     </div>
--- src/lib/utils/classNames.ts
+++ src/lib/utils/classNames.ts
@@ -1,3 +1,10 @@
+import { ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...classes: (string | undefined | false | null)[]): string {
   return classes.filter(Boolean).join(" ");
+}
+
+export function cn2(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
 }
